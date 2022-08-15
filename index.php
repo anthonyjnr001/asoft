37561cca10186f358a22bb1b9638590f64453007
Anthonyjnr <anthonykosisochukwu2002@gmail.com>
2022-08-15 21:45:10 +0100

    Done with css

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link  type="text/css" rel="stylesheet" href="list.css">
+</head>
+<body bgcolor="yellow">
+     <h1>list of places</h1> 
+     <ol>
+        <li>onitsha</li>
+        <li>awka</li>
+        <li>owerri</li>
+        <li>bayelsa</li>
+     </ol>
+     <p><a href="index2.php">clich here for next page</a></p>  
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="table.css">
+</head>
+<body>
+    <table celpadding="5" border="3" cellspacing="0" Align="center" >
+        <tr>
+        <th>Name</th>
+        <th>Image</th>
+        <th>Description</th>
+        <th>Price</th>
+        <th>Add Cart</th>
+        </tr>
+        <tr>
+            <td>DIOR</td>
+            <td><img src="assets/images/dior.png" alt="dior" width="100px" height="100px"></td>
+            <td>BRAND CHRISTIAN DIOR, 
+                COLOR WHITE, 
+                MATERIAL COTTON</td>
+            <td>$50</td>
+            <td><input type="radio"></td>
+        </tr>
+        <tr>
+            <td>LOUIS VUITTON</td>
+            <td bgcolor="white"><img src="assets/images/lv.webp" alt="lv" width="100px" height="100px"></td>
+            <td>MONOGRAM LV TOILE MILITARY JACKET</td>
+            <td>$70</td>
+            <td><input type="radio"></td>
+        </tr>
+        <tr>
+            <td>GUCCI</td>
+            <td><img src="assets/images/gucci.webp" alt="gucci" width="100px" height="100px"></td>
+            <td>OVERSIZED COTTON T-SHIRT WITH LOGO</td>
+            <td>$1000</td>
+            <td><input type="radio"></td>
+        </tr>
+        <tr>
+            <td>FENDI</td>
+            <td><img src="assets/images/fendi.png" alt="fendi" width="100px" height="100px"></td>
+            <td>FENDI WHITE ZIPPED-MEN'S-POLY JACKET</td>
+            <td>$100</td>
+            <td><input type="radio"></td>
+        </tr>
+        <tr>
+            <td>BOTTEGA VENETA</td>
+            <td><img src="assets/images/bottega.webp" alt="bottega" width="100px" height="100px"></td>
+            <td>INTRECCIATO TOWELLING T-SHIRT</td>
+            <td>$200</td>
+            <td><input type="radio"></td>
+        </tr>
+        <tr>
+            <td>ASHLUXE</td>
+            <td><img src="assets/images/ashluxe.webp" alt="ashluxe" width="100px" height="100px"></td>
+            <td>BLACK UNDERGROUND SOUND HOODIE</td>
+            <td>$2000</td>
+            <td><input type="radio"></td>
+        </tr>
+    </table>
+    <p><a href="index.php">back to home</a></p>
+</body>
+</html>
